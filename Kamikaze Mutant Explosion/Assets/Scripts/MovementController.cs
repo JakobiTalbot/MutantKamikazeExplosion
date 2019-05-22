40eac7edb562b924eaf2bcd5abf683e490322cf8
JakobiTalbot <36465136+JakobiTalbot@users.noreply.github.com>
2019-05-22 12:13:38 +1000

    - Completed MovementController.cs

--- Kamikaze Mutant Explosion/Assets/Scripts/MovementController.cs
+++ Kamikaze Mutant Explosion/Assets/Scripts/MovementController.cs
@@ -7,21 +7,29 @@ public class MovementController : MonoBehaviour
     public GameObject[] m_points;
     public float m_moveSpeed = 1f;
 
-    private float m_fLerpTime;
+    private float m_fLerpTime = 0f;
     private bool m_bMovingPoints = false;
     private int m_iCurrentPoint = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         if (m_bMovingPoints)
         {
+            // increase lerp time
             m_fLerpTime += Time.deltaTime * m_moveSpeed;
+
+            // set lerp time to 1 and moving points to false if we have moved all the way
+            if (m_fLerpTime >= 1f)
+            {
+                m_fLerpTime = 1f;
+                m_bMovingPoints = false;
+            }
+
+            // lerp position
+            transform.position = Vector3.Lerp(m_points[m_iCurrentPoint - 1].transform.position, m_points[m_iCurrentPoint].transform.position, m_fLerpTime);
+            // lerp rotation
+            transform.rotation = Quaternion.Lerp(m_points[m_iCurrentPoint - 1].transform.rotation, m_points[m_iCurrentPoint].transform.rotation, m_fLerpTime);
         }
     }
 
