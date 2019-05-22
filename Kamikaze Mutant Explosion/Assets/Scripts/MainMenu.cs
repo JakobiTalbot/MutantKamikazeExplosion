234c6d1db9c3646d0a59210984782d33778bb489
JakobiTalbot <36465136+JakobiTalbot@users.noreply.github.com>
2019-05-23 09:54:33 +1000

    - Implemented main menu scene

---
+++ Kamikaze Mutant Explosion/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    /*  @brief The function called when the start button is pressed, loads the game scene
+    */
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    /*  @brief The function called when the quit button is pressed, exits the application
+    */
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
