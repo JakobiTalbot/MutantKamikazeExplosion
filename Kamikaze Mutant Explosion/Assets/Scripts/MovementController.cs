3488007ca784c011dcfe7e362d32b6d25b738b76
JakobiTalbot <36465136+JakobiTalbot@users.noreply.github.com>
2019-05-22 12:30:40 +1000

    - Completed MovementController.cs

--- Kamikaze Mutant Explosion/Assets/Scripts/MovementController.cs
+++ Kamikaze Mutant Explosion/Assets/Scripts/MovementController.cs
@@ -20,27 +20,52 @@ public class MovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // move to next point if moving points
         if (m_bMovingPoints)
         {
             // increase lerp time
             m_fLerpTime += Time.deltaTime * m_moveSpeed;
 
-            // set lerp time to 1 and moving points to false if we have moved all the way
+            // set lerp time to 0 and moving points to false if we have moved all the way
             if (m_fLerpTime >= 1f)
             {
                 m_fLerpTime = 1f;
                 m_bMovingPoints = false;
             }
 
+            int iPrevPoint;
+            // set index of previous point
+            if (m_iCurrentPoint != 0)
+            {
+                iPrevPoint = m_iCurrentPoint - 1;
+            }
+            else
+            {
+                iPrevPoint = m_points.Length - 1;
+            }
             // lerp position
-            transform.position = Vector3.Lerp(m_points[m_iCurrentPoint - 1].transform.position, m_points[m_iCurrentPoint].transform.position, m_fLerpTime);
+            transform.position = Vector3.Lerp(m_points[iPrevPoint].transform.position, m_points[m_iCurrentPoint].transform.position, m_fLerpTime);
             // lerp rotation
-            transform.rotation = Quaternion.Lerp(m_points[m_iCurrentPoint - 1].transform.rotation, m_points[m_iCurrentPoint].transform.rotation, m_fLerpTime);
+            transform.rotation = Quaternion.Lerp(m_points[iPrevPoint].transform.rotation, m_points[m_iCurrentPoint].transform.rotation, m_fLerpTime);
+        }
+
+        // debug
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            MoveToNextPoint();
         }
     }
 
     public void MoveToNextPoint()
     {
-        m_iCurrentPoint++;
+        // if at end of array, move to first point, otherwise move to next point
+        if (m_iCurrentPoint == m_points.Length - 1)
+            m_iCurrentPoint = 0;
+        else
+            m_iCurrentPoint++;
+        // reset lerp time
+        m_fLerpTime = 0f;
+        // enable moving points
+        m_bMovingPoints = true;
     }
 }
