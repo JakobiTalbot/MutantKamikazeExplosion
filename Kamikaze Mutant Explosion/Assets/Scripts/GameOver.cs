6f2b1f3aebefe00f308b9035c71bdb822b8383f6
JakobiTalbot <36465136+JakobiTalbot@users.noreply.github.com>
2019-05-24 11:38:42 +1000

    - Added GameOver.cs

---
+++ Kamikaze Mutant Explosion/Assets/Scripts/GameOver.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    // reference to the score to be displayed when the game ends
+    public Text m_scoreText;
+
+    /*  @brief The function called when the retry button is pressed; restarts the level
+    */
+    public void Retry()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    /*  @brief The function called when the main menu button is pressed; returns to the main menu
+    */
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    /*  @brief The function called when the quit button is pressed; exits the game
+    */
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
